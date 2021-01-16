bcdb0cecbb419cbc616867d1ae5a66037d3d7708
carcruz <me@carcruzcast.com>
2021-01-16 00:20:59 -0600

    feat: export Bar for data access

---
+++ src/components/BarChart/Bar.js
@@ -0,0 +1,7 @@
+import { Bar as RechartsBar } from "recharts";
+
+/**
+ *  NOTE: use the recharts Bar as it is, here is posible
+ *  to nodify or secure the props that will been pass to the component
+ */
+export default RechartsBar;
