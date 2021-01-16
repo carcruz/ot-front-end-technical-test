181b7abc3fd99c8f7f89218e241eda366c289365
carcruz <me@carcruzcast.com>
2021-01-16 00:18:37 -0600

    feat: structure of main App comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+import GeneInfo from "./pages/GeneInfoPage";
+import { ThemeProvider } from "styled-components";
 
+const theme = {
+  fg: "palevioletred",
+  bg: "white"
+};
+
+/**
+ * @description initialization query client for react-query
+ * @see https://react-query.tanstack.com/reference/QueryClient
+ */
+const queryClient = new QueryClient();
+
+/**
+ * App
+ * @description Main App component that handles main routes and HOC providers
+ */
 function App() {
   return (
-    <div className="App">
-      <p>
-        Feel free to edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <GeneInfo />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
